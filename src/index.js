24d8f1ccaf774428163f1b763a966f78b91a72c2
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2017-01-28 17:34:07 -0500

    login and authtoken mutations are posting to nginx web server

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@
 
 const app = require('./app');
 const port = app.get('port');
+
+console.log(process.env.WEB_ADDRESS_EXT)
+console.log(process.env.WEB_ADDRESS_INT)
+console.log(process.env.WEB_ADDRESSES_EXT)
+console.log(process.env.WEB_ADDRESSES_INT)
+
 console.log(process.env.PORT)
+
 console.log(process.env.POSTGRESQL_ADDRESS)
 console.log(process.env.POSTGRESQL_DATABASE)
 console.log(process.env.POSTGRESQL_USERNAME)
-console.log(process.env.WEB_ADDRESSES)
+
+
 const server = app.listen(process.env.PORT || port);
 
 server.on('listening', () =>
