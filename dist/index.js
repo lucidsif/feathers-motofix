f2422304225667596a35b88dc3c4d19d03cdb9a4
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2017-01-28 16:51:56 -0500

    reverted to pre-sequelize bug and modified config to deploy to digital ocean

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,11 @@
 'use strict';
 const app = require('./app');
 const port = app.get('port');
-const server = app.listen(port);
+console.log(process.env.PORT);
+console.log(process.env.POSTGRESQL_ADDRESS);
+console.log(process.env.POSTGRESQL_DATABASE);
+console.log(process.env.POSTGRESQL_USERNAME);
+console.log(process.env.WEB_ADDRESSES);
+const server = app.listen(process.env.PORT || port);
 server.on('listening', () => console.log(`Feathers application started on ${app.get('host')}:${port}`));
 //# sourceMappingURL=index.js.map
