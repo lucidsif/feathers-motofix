0324d5dd8bce421b3b2459c810c09ea485016476
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2017-01-28 05:09:18 -0500

    pre digitalocean deploy commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@
 
 const app = require('./app');
 const port = app.get('port');
-const server = app.listen(port);
+console.log(process.env.PORT)
+console.log(process.env.POSTGRESQL_ADDRESS)
+console.log(process.env.POSTGRESQL_DATABASE)
+console.log(process.env.POSTGRESQL_USERNAME)
+console.log(process.env.WEB_ADDRESSES)
+const server = app.listen(process.env.PORT || port);
 
 server.on('listening', () =>
   console.log(`Feathers application started on ${app.get('host')}:${port}`)
