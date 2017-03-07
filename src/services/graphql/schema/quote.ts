13fbf8d2ffda65a46d0f11be9b93b05c8c345819
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2017-03-07 04:38:58 -0500

    made fk_user_id and voucher_code_status nonnullable for graphql quote schema

--- src/services/graphql/schema/quote.ts
+++ src/services/graphql/schema/quote.ts
@@ -4,13 +4,13 @@
 export default `
   type Quote implements Node {
     id: ID!
-    fk_user_id: Int
+    fk_user_id: Int!
     motorcycle_json: JSON!
     cart_json: JSON!
     part_json: JSON!
     use_own_parts: Boolean!
-    voucher_code_status: Boolean
+    voucher_code_status: Boolean!
     created_at: String!
-    updated_at: String
+    updated_at: String!
   }
 `
