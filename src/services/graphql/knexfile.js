2dd84ae4cb15ec0ae3c4055b63bea37b8652952c
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2017-04-02 02:14:03 -0400

    harded postgresql url int

--- src/services/graphql/knexfile.js
+++ src/services/graphql/knexfile.js
@@ -1,9 +1,4 @@
 // Update with your config settings.
-console.log('inside knexfile');
-console.log(process.env.POSTGRESQL_URL_INT)
-console.log(process.env.POSTGRESQL_DATABASE)
-console.log(process.env.POSTGRESQL_USERNAME)
-console.log(process.env.POSTGRESQL_PASSWORD)
 
 module.exports = {
 
@@ -40,7 +35,7 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      host: '162.243.45.251',
+      host: '10.128.11.84',
       database: 'motofix_split_backend_production',
       user:     'u78z6y',
       password: 'dpa52pFgTsjbklP'
